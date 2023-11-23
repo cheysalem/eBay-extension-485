393034b93e417ff36b4070b77df8d40bb49fa873
davidkuntz02 <davidkuntz02@gmail.com>
2023-11-22 23:26:51 -0700

    fixed background.js bugs

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
-// NMSU CS485 Fall 2023 Final Project
 
-//this is our background worker
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    console.log("background");
+    
+    // skip urls like "chrome://" to avoid extension error
+    if (tab.url?.startsWith("chrome://")) return undefined;
 
-//const ebay = '*://*.ebay.com/*';
-
-chrome.tabs.onUpdated.addEventListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status == 'complete')
+    if(changeInfo.status == 'complete');
     {
         chrome.scripting.executeScript({
             files: ["script.js"],
             target: {tabId: tab.id}
-        })
+        });
     }
 });
 
-console.log("background");
